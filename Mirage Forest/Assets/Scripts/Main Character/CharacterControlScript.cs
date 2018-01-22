f32e11c8b753a482282db5279f87ac21a90a5787
Hayashi-Tensai <thianchaiphromsuwan@gmail.com>
2018-01-23 01:15:56 +0800

    Very Broken Camera Controls
    x_x

--- Mirage Forest/Assets/Scripts/Main Character/CharacterControlScript.cs
+++ Mirage Forest/Assets/Scripts/Main Character/CharacterControlScript.cs
@@ -7,20 +7,26 @@ public class CharacterControlScript : MonoBehaviour
 
 	Animator anim;
 	Rigidbody rb;
-	Transform character;
+	public Transform character;
+	public Transform model;
 	public float walkingSpeed;
+	public float cameraRotationSpeed;
+	//HT This is to offset the character rotation with the camera
+	float characterRotationOffset;
 	bool isGround;
 
 	void Start ()
 	{
-		character = transform.GetChild(0);
 		anim = character.GetComponent<Animator>();
 		rb = GetComponent<Rigidbody>();
 		isGround = true;
+
+		characterRotationOffset = 0.0f;
 	}
 
 	void Update ()
 	{
+		CameraRotation ();
 		Movement ();
 	}
 
@@ -28,22 +34,22 @@ public class CharacterControlScript : MonoBehaviour
 	{
 		if(Input.GetKey(KeyCode.W) && anim.GetBool("Walk"))
 		{
-			character.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+			character.eulerAngles = new Vector3(0.0f, characterRotationOffset + 0.0f, 0.0f);
 			transform.Translate(Vector3.forward * walkingSpeed * Time.deltaTime);
 		}
 		else if(Input.GetKey(KeyCode.S) && anim.GetBool("Walk"))
 		{
-			character.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
+			character.eulerAngles = new Vector3(0.0f, characterRotationOffset + 180.0f, 0.0f);
 			transform.Translate(Vector3.back * walkingSpeed * Time.deltaTime);
 		}
 		if(Input.GetKey(KeyCode.A) && anim.GetBool("Walk"))
 		{
-			character.eulerAngles = new Vector3(0.0f, -90.0f, 0.0f);
+			character.eulerAngles = new Vector3(0.0f, characterRotationOffset - 90.0f, 0.0f);
 			transform.Translate(Vector3.left * walkingSpeed * Time.deltaTime);
 		}
 		else if(Input.GetKey(KeyCode.D) && anim.GetBool("Walk"))
 		{
-			character.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+			character.eulerAngles = new Vector3(0.0f, characterRotationOffset + 90.0f, 0.0f);
 			transform.Translate(Vector3.right * walkingSpeed * Time.deltaTime);
 		}
 
@@ -69,6 +75,18 @@ public class CharacterControlScript : MonoBehaviour
 		else
 		{
 			anim.SetBool("Jump", false);
+		}
+	}
+
+	void CameraRotation ()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Camera.main.transform.eulerAngles = new Vector3(Camera.main.transform.eulerAngles.x,
+															Camera.main.transform.eulerAngles.y + Input.GetAxis("Mouse X") * cameraRotationSpeed * Time.deltaTime,
+															Camera.main.transform.eulerAngles.z);
+			
+			characterRotationOffset = Camera.main.transform.eulerAngles.y;
 		}
 	}
 
