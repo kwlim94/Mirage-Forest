1b039e735b2aff380c7b771dc75259a6095ba490
Lim Kheng Wei <30492649+kwlim94@users.noreply.github.com>
2018-03-07 04:29:04 +0800

    InteractionObjt
    not complete

--- Mirage Forest/Assets/Scripts/InteractionScript.cs
+++ Mirage Forest/Assets/Scripts/InteractionScript.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class InteractionScript : MonoBehaviour
 {
 	public GameObject player;
+	public GameObject Obj;
 	public bool isInteractable;
+	public bool InteractableObj;
 	public int idNumber;
 
 	void Start ()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
+		Obj = GameObject.FindGameObjectWithTag("InterObj");
+		InteractableObj = false;
 		isInteractable = false;
 	}
 
 	void Update ()
 	{
-		if(isInteractable && Input.GetKeyDown(KeyCode.E))
+		if(isInteractable && InteractableObj && Input.GetKeyDown(KeyCode.E))
 		{
 			Interact ();
 		}
