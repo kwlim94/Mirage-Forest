c346ff69eb305de08927d79942a0102e20a44d69
Lim Kheng Wei <30492649+kwlim94@users.noreply.github.com>
2018-03-14 04:55:58 +0800

    update

--- Mirage Forest/Assets/Scripts/NarrativeControlScript.cs
+++ Mirage Forest/Assets/Scripts/NarrativeControlScript.cs
@@ -33,10 +33,10 @@ public class NarrativeControlScript : MonoBehaviour
 	{
 		if(speechBubble.gameObject.activeSelf)
 		{
-			if(Input.GetMouseButtonDown(0))
-			{
-				NextPage ();
-			}
+//			if(Input.GetMouseButtonDown(0))
+//			{
+//				NextPage ();
+//			}
 				
 			if(timeElasped > tempDialogueList[pageNumber].duration)
 			{
