342afe2e9fc5ec9e72f648e9f5aa72bd4c5685d2
Hayashi-Tensai <thianchaiphromsuwan@gmail.com>
2018-03-13 13:25:56 +0800

    portal Interactiom

---
+++ Mirage Forest/Assets/Scripts/KeyControlScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InputKey
+{
+	public KeyCode keyboardKey;
+	public string buttonKey;
+
+}
+
+public class KeyControlScript : MonoBehaviour
+{
+	public static KeyControlScript Instance {get; set;}
+
+	void Awake()
+	{
+		if(Instance!= null && Instance != this)
+			Destroy(gameObject);
+		else
+			Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public InputKey interactKey;
+}
