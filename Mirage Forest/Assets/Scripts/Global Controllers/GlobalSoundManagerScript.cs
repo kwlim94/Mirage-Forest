b4a33414196ea19df7f2a7ba09a7981e22cf0a5b
Hayashi-Tensai <thianchaiphromsuwan@gmail.com>
2018-05-15 17:34:01 +0800

    Updating some stuff
    Updating the Programming Guide
    Adding a sound script

---
+++ Mirage Forest/Assets/Scripts/Global Controllers/GlobalSoundManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSoundManagerScript : MonoBehaviour
+{
+	public static SceneManagerScript Instance {get; set;}
+
+	void Awake()
+	{
+		if(Instance!= null && Instance != this)
+			Destroy(gameObject);
+		else
+			Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void PlaySound();
+	public void AddLoopingSound(float fadeInDuration);
+	public void StopLoopingSound(float fadeOutDuration);
+	public void DeleteAllSounds(float fadeOutDuration);
+}
